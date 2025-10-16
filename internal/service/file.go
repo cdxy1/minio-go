4c3030d6ed42fb4171d1714761438c7d3ccc5c5d
Artem <dalyvinchi@gmail.com>
2025-10-16 19:13:21 +0300

    ADD file service

--- internal/service/file.go
+++ internal/service/file.go
@@ -1,7 +1,33 @@
 package service
 
-import "github.com/cdxy1/go-file-storage/internal/repo"
+import (
+	"bytes"
+	"context"
+
+	"github.com/cdxy1/go-file-storage/internal/repo"
+	"github.com/minio/minio-go/v7"
+)
 
 type FileService struct {
 	repo *repo.File
+}
+
+func (fs *FileService) DownloadFile(ctx context.Context, objName string) (*minio.Object, error){
+	obj, err := fs.repo.GetByName(ctx, objName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, err
+}
+
+func (fs *FileService) UploadFile(ctx context.Context, objName string, objData []byte) error {
+	rdr := bytes.NewReader(objData)
+	dl := int64(rdr.Len())
+
+	if err := fs.repo.Put(ctx, objName, rdr, dl); err != nil {
+		return err
+	}
+	return nil
 }
